b33b900d09359325eed9197b5eb8510a18b98c66
Carlos Sanchez <carlos.sanchez.marin@alumnos.upm.es>
2019-12-16 18:23:34 +0100

    Completed question 2 graph

--- question_2.R
+++ question_2.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(patchwork)
 
 source("utils/question_2_func.R")
 
@@ -12,7 +13,7 @@ matches <- matches %>% select(-urlTeamSeasonLogo) %>%
   mutate(conferenceMatch = type_match(teams, slugTeam, slugOpponent)) %>%
   ungroup()
 
-match_conference_winner = matches %>% 
+match_conference_winner <- matches %>%
   group_by(idGame) %>% 
   summarise(winner = first(slugTeamWinner),
             loser = first(slugTeamLoser),
@@ -20,3 +21,45 @@ match_conference_winner = matches %>%
   rowwise() %>%
   mutate(conferenceWinner = teams[teams$slugTeam == winner, "conference"]) %>%
   ungroup()
+
+match_team_winner <- match_conference_winner %>%
+  group_by(winner) %>%
+  tally() %>%
+  mutate(count = n) %>%
+  select(-n) %>%
+  ungroup()
+
+match_team_conference_win <- match_conference_winner %>%
+  group_by(winner) %>%
+  count(conferenceMatch) %>%
+  mutate (count = n) %>%
+  select(-n) %>%
+  ungroup()
+
+isSameConferenceMatch <- match_team_conference_win %>%
+  rowwise() %>%
+  mutate(isSame = if(conferenceMatch != "MIXED") "SAME" else "MIXED") %>%
+  left_join(teams, by = c("winner" = "slugTeam")) %>%
+  select(winner, conferenceMatch, count, isSame, conference)
+
+g_east <- isSameConferenceMatch %>% filter(conference == "EAST") %>%
+  ggplot(aes(fill = isSame, y = count, x = winner)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ylim(0, 45) +
+  facet_wrap(~conference)
+g_west <- isSameConferenceMatch %>% filter(conference == "WEST") %>%
+  ggplot(aes(fill = isSame, y = count, x = winner)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ylim(0, 45) +
+  scale_fill_discrete(name = "Conference", labels = c("Opposite", "Same")) +
+  facet_wrap(~conference)
+patchwork <- g_east + g_west
+patchwork + plot_annotation(
+  title = "Wins per team grouped by conference",
+  theme = theme(plot.title = element_text(hjust = 0.45))
+)
