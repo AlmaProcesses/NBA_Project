f20a09dab2897508f9a412a8ba5fbd4b5f1741bb
agonzalezl <angel.gonzalez.lpz@gmail.com>
2019-12-17 18:31:36 +0100

    Added basic linear model

--- playersStatsSalary.R
+++ playersStatsSalary.R
@@ -11,6 +11,8 @@ if (!require("normtest"))
   install.packages("normtest")
 if (!require("tseries"))
   install.packages("tseries")
+if (!require("GGally"))
+  install.packages("GGally")
 
 # Load libraries
 library(ggplot2)
@@ -19,7 +21,7 @@ library(car)
 library(lmtest)
 library(normtest)
 library(tseries)
-
+library("GGally")
 
 setwd("C:/workspace/University/DataProcesses/NBA_Project")
 
@@ -73,7 +75,11 @@ stats_aggr <- stats_players %>%
             pf = mean(pf, na.rm=TRUE),
             treb = mean(treb, na.rm=TRUE),
             fg2m = mean(fg2m, na.rm=TRUE),
-            fg2a = mean(fg2a, na.rm=TRUE))
+            fg2a = mean(fg2a, na.rm=TRUE),
+            fga = mean(fga, na.rm=TRUE),
+            fgm = mean(fgm, na.rm=TRUE),
+            ftm = mean(ftm, na.rm=TRUE))%>%
+  ungroup()
 
 
 merged = merge(salaries_players, stats_aggr, by.x="Player", by.y="namePlayer")
@@ -82,23 +88,29 @@ merged = merge(x = stats_aggr, y = salaries_players[ , c("Player", "Salary")], b
 
 merged = merge(merged, player_data[ , c("namePlayer", "countSeasons", "nameTeam")], by.x="namePlayer", by.y="namePlayer")
 
-merged = merge(merged, teams_data[ , c("nameTeam", "idConference")], by.x="nameTeam", by.y="nameTeam")
+merged = merge(merged, teams_data[ , c("nameTeam", "idConference", "idDivision")], by.x="nameTeam", by.y="nameTeam")
 
 merged$idConference <- as.factor(merged$idConference)
 
+merged$idDivision <- as.factor(merged$idDivision)
+
+merged$logsalary <- log(merged$Salary)
+
+#ggpairs(merged, columns = c("logsalary","treb", "blk", "fgm", "fga",  "winrate", "minutes", "pts", "ast", "countSeasons" ))
+
 ggplot() + 
-  geom_point(data=merged, aes(x=pts, y=ast, colour=Salary)) + 
+  geom_point(data=merged, aes(x=ast, y=pts, colour=Salary)) + 
   theme(legend.position="right") + 
   scale_colour_gradient(low="#19E719", high="#E71919")
 
-
+#
 ggplot() + 
-  geom_point(data=merged, aes(x=pts, y=minutes, colour=Salary)) + 
+  geom_point(data=merged, aes(x=countSeasons, y=minutes, colour=log(Salary) )) + 
   theme(legend.position="right") + 
   scale_colour_gradient(low="#19E719", high="#E71919")
 
+lm1<-lm(log(Salary) ~ minutes + countSeasons  
+         data=merged, x=TRUE, y=TRUE)x
+summary(lm1)
 
-Full<-lm(Salary~pts+countSeasons + plusminus + stl + tov + pf + treb , 
-         data=merged, x=TRUE, y=TRUE)
-summary(Full)
 
