e026529fe6732b3f8409f319d1022258c1c2b4b4
agonzalezl <angel.gonzalez.lpz@gmail.com>
2019-12-18 21:11:03 +0100

    Basic graphs added to Rmarkdown

--- playersStatsSalary.R
+++ playersStatsSalary.R
@@ -1,18 +1,4 @@
 
-if (!require("ggplot2"))
-  install.packages("ggplot2")
-if (!require("dplyr"))
-  install.packages("dplyr")
-if (!require("ElemStatLearn"))
-  install.packages("car")
-if (!require("lmtest"))
-  install.packages("lmtest")
-if (!require("normtest"))
-  install.packages("normtest")
-if (!require("tseries"))
-  install.packages("tseries")
-if (!require("GGally"))
-  install.packages("GGally")
 
 # Load libraries
 library(ggplot2)
@@ -21,42 +7,21 @@ library(car)
 library(lmtest)
 library(normtest)
 library(tseries)
-library("GGally")
+library(GGally)
 
 setwd("C:/workspace/University/DataProcesses/NBA_Project")
 
-salaries_players <-
-  read.delim(
-    './data/salaries_player.csv',
-    sep = ",",
-    header = TRUE,
-    dec = "."
-  )
-
-stats_players <-
-  read.delim(
-    './data/logs_players.csv',
-    sep = ",",
-    header = TRUE,
-    dec = "."
-  )
-
-player_data <-
-  read.delim(
-    './data/players.csv',
-    sep = ",",
-    header = TRUE,
-    dec = "."
-  )
+salaries_players <-read.csv("./data/salaries_player.csv", stringsAsFactors = F)
 
 
-teams_data <-
-  read.delim(
-    './data/nba_teams.csv',
-    sep = ",",
-    header = TRUE,
-    dec = "."
-  )
+stats_players <-read.csv("./data/logs_players.csv", stringsAsFactors = F)
+
+
+player_data <-read.csv("./data/players.csv", stringsAsFactors = F)
+
+
+teams_data <-read.csv("./data/nba_teams.csv", stringsAsFactors = F)
+
 
 stats_aggr <- stats_players %>%
   group_by(namePlayer) %>%
@@ -86,7 +51,7 @@ merged = merge(salaries_players, stats_aggr, by.x="Player", by.y="namePlayer")
 
 merged = merge(x = stats_aggr, y = salaries_players[ , c("Player", "Salary")], by.x="namePlayer", by.y="Player")
 
-merged = merge(merged, player_data[ , c("namePlayer", "countSeasons", "nameTeam")], by.x="namePlayer", by.y="namePlayer")
+merged = merge(merged, player_data[ , c("namePlayer", "countSeasons", "nameTeam", "idLeagueOtherExperience")], by.x="namePlayer", by.y="namePlayer")
 
 merged = merge(merged, teams_data[ , c("nameTeam", "idConference", "idDivision")], by.x="nameTeam", by.y="nameTeam")
 
@@ -94,23 +59,12 @@ merged$idConference <- as.factor(merged$idConference)
 
 merged$idDivision <- as.factor(merged$idDivision)
 
+merged$idLeagueOtherExperience <- as.factor(merged$idLeagueOtherExperience)
+
 merged$logsalary <- log(merged$Salary)
+merged$sqrtsalary <- sqrt(merged$Salary)
 
-#ggpairs(merged, columns = c("logsalary","treb", "blk", "fgm", "fga",  "winrate", "minutes", "pts", "ast", "countSeasons" ))
 
-ggplot() + 
-  geom_point(data=merged, aes(x=ast, y=pts, colour=Salary)) + 
-  theme(legend.position="right") + 
-  scale_colour_gradient(low="#19E719", high="#E71919")
-
-#
-ggplot() + 
-  geom_point(data=merged, aes(x=countSeasons, y=minutes, colour=log(Salary) )) + 
-  theme(legend.position="right") + 
-  scale_colour_gradient(low="#19E719", high="#E71919")
-
-lm1<-lm(log(Salary) ~ minutes + countSeasons  
-         data=merged, x=TRUE, y=TRUE)x
-summary(lm1)
+##ggpairs(merged, columns = c("sqrtsalary", "blk", "minutes", "pts", "ast",  "idConference" ))
 
 
